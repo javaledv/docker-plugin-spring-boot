cc7eff82cb05367e8a12ce183abf667f2e0b5d73
denis <denislesheniuk@gmail.com>
2018-01-31 08:03:21 +0300

    Implementing generation Dockerfile from gradle.build.

--- src/main/java/ml/ledv/docker/plugin/spring/boot/Main.java
+++ src/main/java/ml/ledv/docker/plugin/spring/boot/Main.java
@@ -1,4 +1,20 @@
 package ml.ledv.docker.plugin.spring.boot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
 public class Main {
+
+    public static void main(String args []){
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World";
+    }
 }
